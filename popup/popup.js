80b1ff22fb8048033bb461c42d28fc0f59d2175a
Telenchak Oleksiy <letel315@gmail.com>
2023-11-06 00:12:07 +0200

    start styling

--- popup/popup.js
+++ popup/popup.js
@@ -1,8 +1,15 @@
 function updateTime() {
-  chrome.storage.local.get(["timer"], (res) => {
+  chrome.storage.local.get(["timer", "timeOption"], (res) => {
     const time = document.getElementById("time");
-    time.textContent = res.timer;
-    console.log(res.timer);
+    const minutes = `${res.timeOption - Math.ceil(res.timer / 60)}`.padStart(
+      2,
+      "0"
+    );
+    let seconds = "00";
+    if (res.timer % 60 !== 0) {
+      seconds = `${60 - (res.timer % 60)}`.padStart(2, "0");
+    }
+    time.textContent = `${minutes}:${seconds}`;
   });
 }
 
